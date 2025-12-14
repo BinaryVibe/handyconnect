a9b149d0d45bc8461fcf070e7e4602de27fff6f3
NextStep-IM <malikharis2003@gmail.com>
2025-12-14 19:57:15 +0500

    Use go_router for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'signup.dart'; // For testing
+import 'package:go_router/go_router.dart';
+
+// For testing
+import 'signup.dart';
 import 'worker_dashboard.dart';
 import 'customer_dashboard.dart';
 import 'login.dart';
+import 'homepage.dart';
 
-const Color kPrimaryColor = Color.fromARGB(
-  255,
-  74,
-  46,
-  30,
+const Color kPrimaryColor = Color.fromARGB(255, 74, 46, 30);
+
+final _router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    GoRoute(path: '/login', builder: (context, state) => const LoginScreen(),),
+    GoRoute(path: '/signup', builder: (context, state) => const SignUpScreen(),),
+    GoRoute(path: '/customer-dashboard', builder: (context, state) => const CustomerDashboard(),),
+    GoRoute(path: '/worker-dashboard', builder: (context, state) => const WorkerDashboard(),),
+  ],
 );
 
 void main() async {
@@ -26,13 +36,11 @@ class HandyConnect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: kPrimaryColor,
-      ),
+    return MaterialApp.router(
+      routerConfig: _router,
+      theme: ThemeData(primaryColor: kPrimaryColor),
       debugShowCheckedModeBanner: false,
       title: 'HandyConnect',
-      home: const SignUpScreen(),
     );
   }
 }
